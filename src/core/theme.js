1827e63277ea89702b409e109feab5c410daf4d3
Alexandre Leblanc <alexandreleblanc89@gmail.com>
2020-12-22 01:49:50 -0500

    fixing push

---
+++ src/core/theme.js
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'react-native-paper'
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    text: '#000000',
+    textwhite: '#FFFFFF',
+    primary: '#0B64A0',
+    secondary: '#970204',
+    error: '#f13a59',
+  },
+}
